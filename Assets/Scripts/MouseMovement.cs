cc32199935447cc68e3ff795555371035b252c55
Senterrr <48488790+Senterrr@users.noreply.github.com>
2021-02-03 15:33:32 +0100

    Small Adjustments

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class MouseMovement : MonoBehaviour
 {
     public float MoveSpeed = 1;
 
     // Link it in unity
-    [SerializeField] Animation animation;
+    [SerializeField] Sprite[] sprites;
 
+    public SpriteRenderer spriteholder;
     private List<GameObject> CollisionList;
     private float mouseposX;
+
+    private bool bonked;
     private Vector3 rand;
+    [SerializeField] float TimeBetweenFrames;
+
     private void Start()
     {
         Cursor.visible = false;
@@ -21,29 +25,51 @@ public class MouseMovement : MonoBehaviour
 
     private void Update()
     {
-        RaycastHit rayHit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity))
+        if (!bonked)
         {
-            mouseposX = rayHit.point.x;
-        }
-        transform.position = new Vector3(Mathf.Lerp(transform.position.x, mouseposX, MoveSpeed * Time.deltaTime), 0, 0);
+            RaycastHit rayHit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity))
+            {
+                mouseposX = rayHit.point.x;
+            }
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, mouseposX, MoveSpeed * Time.deltaTime), 0, 0);
 
-        if (Input.GetMouseButtonUp(0))
-            Bonk();
+            if (Input.GetMouseButtonUp(0))
+                Bonk();
+        }
+
+
+    }
+
+    private IEnumerator animator()
+    {
+        bonked = true;
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            spriteholder.sprite = sprites[i];
+
+            if (i > 1 && i < 5) { spriteholder.sortingOrder = 4; }
+
+            else { spriteholder.sortingOrder = 1; }                
+            
+            yield return new WaitForSeconds(TimeBetweenFrames);          
+        }
+        bonked = false;
     }
 
     private void Bonk()
     {
+        StartCoroutine(animator());
         //start animation
 
         //start this code once animation is finished. (change the 0 value to hammer hit)
-        if (animation.clip.length == 0f)
-        {
-            foreach  (GameObject collider in CollisionList)
-            {
-                collider.SetActive(false);
-            }
-        }
+
+
+        //foreach (GameObject collider in CollisionList)
+        //{
+        //    collider.SetActive(false);
+        //}
+
     }
 
 
@@ -56,5 +82,6 @@ public class MouseMovement : MonoBehaviour
     {
         CollisionList.Remove(other.gameObject);
     }
+
 
 }
